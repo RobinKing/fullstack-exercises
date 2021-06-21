6d8886e263cfaaa44b6accd0252c649f108c0887
robin <jingjl1001@gmail.com>
2021-06-21 22:48:21 +0800

    <After ex2.7>Step 2, prompt when name dumplicated.

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -8,38 +8,42 @@ const App = () => {
 
   const addPerson = (event) => {
     event.preventDefault()
-    const personObject = {
-      name: newName
-    }
 
-    setPersons(persons.concat(personObject))
-    setNewName('')
+    if (-1 === persons.findIndex((p) => p.name === newName)) {
+      const personObject = {
+        name: newName
+      }
+      setPersons(persons.concat(personObject))
+      setNewName('')
+    }
+    else
+      alert(`${newName} is already added to phonebook`)
   }
 
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <form>
-        <div>
-          name: <input
-            value={newName}
-            onChange={(event) => setNewName(event.target.value)}
-          />
-        </div>
-        <div>
-          <button
-            type="submit"
-            onClick={addPerson}
-          >add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      {persons.map((person) => (
-        <li key={person.name}>{person.name}</li>
-      )
-      )}
-    </div>
-  )
+return (
+  <div>
+    <h2>Phonebook</h2>
+    <form>
+      <div>
+        name: <input
+          value={newName}
+          onChange={(event) => setNewName(event.target.value)}
+        />
+      </div>
+      <div>
+        <button
+          type="submit"
+          onClick={addPerson}
+        >add</button>
+      </div>
+    </form>
+    <h2>Numbers</h2>
+    {persons.map((person) => (
+      <li key={person.name}>{person.name}</li>
+    )
+    )}
+  </div>
+)
 }
 
 export default App
