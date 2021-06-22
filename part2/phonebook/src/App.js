7cdd41f6f26ae68d9d488184c90a2a35895f6f2f
robin <jingjl1001@gmail.com>
2021-06-22 22:02:12 +0800

    <After ex2.8>Step3, could add number and fix the layout difference.

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,48 +2,60 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    {
+      name: 'Arto Hellas',
+      number: '040-1234567'
+    }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
 
     if (-1 === persons.findIndex((p) => p.name === newName)) {
       const personObject = {
-        name: newName
+        name: newName,
+        number: newNumber
       }
       setPersons(persons.concat(personObject))
       setNewName('')
+      setNewNumber('')
     }
     else
       alert(`${newName} is already added to phonebook`)
   }
 
-return (
-  <div>
-    <h2>Phonebook</h2>
-    <form>
-      <div>
-        name: <input
-          value={newName}
-          onChange={(event) => setNewName(event.target.value)}
-        />
-      </div>
-      <div>
-        <button
-          type="submit"
-          onClick={addPerson}
-        >add</button>
-      </div>
-    </form>
-    <h2>Numbers</h2>
-    {persons.map((person) => (
-      <li key={person.name}>{person.name}</li>
-    )
-    )}
-  </div>
-)
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input
+            value={newName}
+            onChange={(event) => setNewName(event.target.value)}
+          />
+        </div>
+        <div>
+          number: <input
+            value={newNumber}
+            onChange={(event) => setNewNumber(event.target.value)}
+          />
+        </div>
+        <div>
+          <button
+            type="submit"
+            onClick={addPerson}
+          >add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <p key={person.name}>{person.name} {person.number}</p>
+      )
+      )}
+    </div>
+  )
 }
 
 export default App
